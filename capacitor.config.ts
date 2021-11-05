f9ebb691eabd603ef468016ce298719febe7bff9
Devin Shoemaker <devinshoe@gmail.com>
2021-11-05 09:45:45 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'global-cap-test',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
